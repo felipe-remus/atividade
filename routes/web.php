f605fc8fb4aa91f15e199a9bef678cafb0f5dc35
felipe-remus <felipe-remus@email>
2026-05-06 15:20:06 -0300

    v7

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,15 @@ Route::match(['get', 'post'], '/menu', function () {
     return view('menu');
 });
 
-Route::get('/alimentos/{id}/edit', [AlimentoController::class, 'edit'])->name('edit');
-Route::put('/alimentos/{id}', [AlimentoController::class, 'update'])->name('update');
-Route::delete('/alimentos/{id}', [AlimentoController::class, 'destroy'])->name('destroy');
+Route::get('/listagem', [AlimentoController::class, 'index'])->name('listagem');
 
 Route::get('/alimentos/{id}', [AlimentoController::class, 'show'])->name('show');
 
-Route::get('/listagem', [AlimentoController::class, 'index'])->name('listagem');
 Route::get('/cadastro', [AlimentoController::class, 'create'])->name('create');
+
+Route::get('/alimentos/{id}/edit', [AlimentoController::class, 'edit'])->name('edit');
+
 Route::post('/alimentos', [AlimentoController::class, 'store'])->name('store');
+
+Route::put('/alimentos/{id}', [AlimentoController::class, 'update'])->name('update');
+Route::delete('/alimentos/{id}', [AlimentoController::class, 'destroy'])->name('destroy');
